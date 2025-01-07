4f4c4e9fc465f8817d572289f9d82d6b7f7bbea3
Hanane kh <143598467+Hanane8@users.noreply.github.com>
2025-01-07 20:11:42 +0100

    #9

---
+++ Application Layer/Helpers/TokenHelper.cs
@@ -0,0 +1,47 @@
+﻿using Domain_Layer.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application_Layer.Helpers
+{
+    public class TokenHelper
+    {
+        IConfiguration _configuration;
+        private readonly UserManager<User> _userManager;
+        public TokenHelper(IConfiguration configuration, UserManager<User> userManager)
+        {
+            _configuration = configuration;
+            _userManager = userManager;
+        }
+        public async Task<string> GenerateJwtTokenAsync(User user)
+        {
+            var roles = await _userManager.GetRolesAsync(user);
+            var claims = new List<Claim>();
+
+            foreach (var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+
+            claims.Add(new Claim(ClaimTypes.Email, user.Email));
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature);
+            var token = new JwtSecurityToken(_configuration["Jwt:Issuer"],
+                _configuration["Jwt:Audience"],
+                claims,
+                expires: DateTime.UtcNow.AddHours(1),
+                signingCredentials: creds);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
