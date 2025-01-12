938e1cbac8d2a29254b5735d1454b7bf09ff4ee1
Hanane kh <143598467+Hanane8@users.noreply.github.com>
2025-01-12 09:47:13 +0100

    Create DTO #37

--- Application Layer/DTO/OrderDTO/OrderItemDTO.cs
+++ Application Layer/DTO/OrderDTO/OrderItemDTO.cs
@@ -4,15 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Application_Layer.DTO
+namespace Application_Layer.DTO.OrderDTO
 {
     public class OrderItemDTO
     {
+        public int Id { get; set; }
+        public int OrderId { get; set; }
         public Guid ProductId { get; set; }
         public string ProductName { get; set; } = string.Empty;
+        public string? Size { get; set; }
         public int Quantity { get; set; }
         public decimal Price { get; set; }
-        public decimal TotalPrice => Price * Quantity; 
+        public decimal TotalPrice => Price * Quantity;
     }
 
 }
